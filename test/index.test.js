7fb67535b92062f56716d3bf04fd3feff9ba40b2
Chris Shum <chris.shum@mongodb.com>
2016-07-06 16:36:55 -0400

    WIP correctly access intercom API to get users

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,40 @@
-var intercomApiWrapper = require('../');
-var assert = require('assert');
+var intercomApiWrapper = require('../lib/index.js');
+// var assert = require('assert');
+var expect = require('chai').expect;
+// var should = require('chai').should;
+// var sinon = require('sinon');
 
-describe('intercom-api-wrapper', function(){
-  it('should work', function(){
-    assert(intercomApiWrapper);
+// console.log(intercomApiWrapper.getUsers)
+// console.log(intercomApiWrapper.getUsers(1));
+// console.log(intercomApiWrapper.getUsersBySegment)
+
+
+describe('intercom-api-wrapper', function() {
+  it('should exist', function() {
+    expect(intercomApiWrapper).to.exist;
+  });
+
+  it('should return a stream', function() {
+    expect(intercomApiWrapper.getUsers, 1).to.be.a('stream');
+  });
+
+  it('should throw no segment provided error', function() {
+    expect(intercomApiWrapper.getUsersBySegment).to.throw(/NO SEGMENT PROVIDED/);
+  });
+
+  it('should throw segment does not exist error', function() {
+    expect(intercomApiWrapper.getUsersBySegment, 'bad segment').to.throw(/SEGMENT PROVIDED DOES NOT EXIST/);
+  });
+
+  it('should throw segment does not exist error', function() {
+    expect(intercomApiWrapper.getUsersBySegment, 1).to.throw(/SEGMENT PROVIDED DOES NOT EXIST/);
+  });
+
+  it('should throw segment does not exist error', function() {
+    expect(intercomApiWrapper.getUsersBySegment, 'bad segment', 1).to.throw(/SEGMENT PROVIDED DOES NOT EXIST/);
+  });
+
+  it('should return a stream', function() {
+    expect(intercomApiWrapper.getUsersBySegment('Active', 1)).to.be.a('stream');
   });
 });
