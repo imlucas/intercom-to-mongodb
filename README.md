cfd4ea4d78fbce04a7f889cd759099469aca8401
Chris Shum <chris.shum@mongodb.com>
2016-07-13 12:02:59 -0400

    INT-1613 Module to wrap Intercom API (#1)
    * WIP correctly access intercom API to get users
    
    * WIP removed commented code for debugging
    
    * WIP added getCounts
    
    * function testing
    
    * correctness and functionality testing, refactor test options to seperate file for clarity
    
    * README
    
    * minor README updates
    
    * small error in equality testing
    
    * added dotenv, removed comments

--- README.md
+++ README.md
@@ -1,11 +1,24 @@
 # intercom-api-wrapper [![travis][travis_img]][travis_url] [![npm][npm_img]][npm_url]
 
-> Light wrapper around intercom.io node driver to abstract out modintercom-specific functions
+> Light wrapper around intercom.io's API requests to abstract out intercom-specific methods
 
 ## Example
+The wrapper exposes three basic functions `getUsers`, `getUsersBySegment`, and `getCount`.
 
 ```javascript
+getUsers(<lastNumDays>, callback);
 ```
+`getUsers` returns a list of users. Expects an integer for `lastNumDays` and a callback - `lastNumDays` limits the return results to only users created in the last `lastNumDays` days, omit this parameter to get all users ever created.
+
+```javascript  
+getUsersBySegment(<segment>, callback);
+```
+`getUsersBySegment` returns a list of users that are in a specified segment. Expects a string for `segment` and a callback - `segment` should be an existing segment in Intercom.
+
+```javascript
+getCount(<type>, <count>, callback);
+```
+`getCount` returns a list of key + count pairs, Expects a string for `type` and `count` and a callback - `type` determines the unit of grouping (company, segment, tag, users) and `count` determines how to bucket the units
 
 ## License
 
--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,108 @@
-var intercomApiWrapper = require('../');
-var assert = require('assert');
+/* eslint no-unused-expressions:0 */
 
-describe('intercom-api-wrapper', function(){
-  it('should work', function(){
-    assert(intercomApiWrapper);
+var intercomApiWrapper = require('../lib/index.js');
+var testVars = require('./testVars.js');
+var request = require('request');
+var expect = require('chai').expect;
+var sinon = require('sinon');
+
+describe('intercom api wrapper testing', function() {
+  before(function(done) {
+    var stub = sinon.stub(request, 'get');
+    stub.withArgs(testVars.usersOptions).yields(null, { statusCode: '200' }, testVars.usersOutput);
+    stub.withArgs(testVars.usersCreatedSinceOptions).yields(null, { statusCode: '200' }, testVars.usersOutput);
+    stub.withArgs(testVars.usersBySegmentOptions).yields(null, { statusCode: '200' }, testVars.usersOutput);
+    stub.withArgs(testVars.segmentsOptions).yields(null, { statusCode: '200' }, testVars.segmentsOutput);
+    stub.withArgs(testVars.countOptions).yields(null, { statusCode: '200' }, testVars.countsOutput);
+    done();
+  });
+
+  after(function(done) {
+    request.get.restore();
+    done();
+  });
+
+  it('api should exist', function() {
+    expect(intercomApiWrapper).to.exist;
+  });
+
+  it('getUsers with no arguments should return a stream', function(done) {
+    intercomApiWrapper.getUsers(null, function(err, result) {
+      if (err) return done(err);
+      expect(result).to.be.an('object');
+      expect(result.__HighlandStream__).to.be.true;
+      result.on('data', function(data) {
+        expect(data).to.equal(testVars.usersOutput);
+      });
+      done();
+    });
+  });
+
+  it('getUsers with lastNumDays should return a stream', function(done) {
+    intercomApiWrapper.getUsers(1, function(err, result) {
+      if (err) return done(err);
+      expect(result).to.be.an('object');
+      expect(result.__HighlandStream__).to.be.true;
+      result.on('data', function(data) {
+        data.to.equal(testVars.usersOutput);
+      });
+      done();
+    });
+  });
+
+  it('getUsersBySegment with no segment should return no segment provided error', function(done) {
+    intercomApiWrapper.getUsersBySegment(null, function(err, result) {
+      expect(err).to.equal('NO SEGMENT PROVIDED');
+      expect(result).to.be.undefined;
+      done();
+    });
+  });
+
+  it('getUsersBySegment with bad segment should return segment does not exist error', function(done) {
+    intercomApiWrapper.getUsersBySegment('bad segment', function(err, result) {
+      expect(err).to.equal('SEGMENT PROVIDED DOES NOT EXIST');
+      expect(result).to.be.undefined;
+      done();
+    });
+  });
+
+  it('getUsersBySegment with correct segment should return a stream', function(done) {
+    intercomApiWrapper.getUsersBySegment('test', function(err, result) {
+      if (err) return done(err);
+      expect(result).to.be.an('object');
+      expect(result.__HighlandStream__).to.be.true;
+      result.on('data', function(data) {
+        expect(data).to.equal(testVars.usersOutput);
+      });
+      done();
+    });
+  });
+
+  it('getCount with no type should return no type provided error', function(done) {
+    intercomApiWrapper.getCount(null, 'test', function(err, result) {
+      expect(err).to.equal('NO TYPE OR COUNT PROVIDED');
+      expect(result).to.be.undefined;
+      done();
+    });
+  });
+
+  it('getCount with no count should return no count provided error', function(done) {
+    intercomApiWrapper.getCount('test', null, function(err, result) {
+      expect(err).to.equal('NO TYPE OR COUNT PROVIDED');
+      expect(result).to.be.undefined;
+      done();
+    });
+  });
+
+  it('getCount with correct type and count should return a stream', function(done) {
+    intercomApiWrapper.getCount('user', 'segment', function(err, result) {
+      if (err) return done(err);
+      expect(result).to.be.an('object');
+      expect(result.__HighlandStream__).to.be.true;
+      result.on('data', function(data) {
+        expect(data).to.equal(testVars.countsOutput);
+      });
+      done();
+    });
   });
 });
