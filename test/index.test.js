b0320d4926c9caba2f54241f5b30276447beebc0
Chris Shum <chris.shum@mongodb.com>
2016-07-19 16:54:51 -0400

    change params for getData(); functionality testing
    getData now expects an object for its first argument instead of a string - change necessary for unpacking the response from intercom

--- test/index.test.js
+++ test/index.test.js
@@ -6,109 +6,153 @@ var request = require('request');
 var expect = require('chai').expect;
 var sinon = require('sinon');
 
-describe('intercom api wrapper testing', function() {
-  before(function(done) {
-    var stub = sinon.stub(request, 'get');
-    stub.withArgs(testVars.usersOptions).yields(null, { statusCode: '200' }, testVars.usersOutput);
-    stub.withArgs(testVars.usersCreatedSinceOptions).yields(null, { statusCode: '200' }, testVars.usersOutput);
-    stub.withArgs(testVars.usersBySegmentOptions).yields(null, { statusCode: '200' }, testVars.usersOutput);
-    stub.withArgs(testVars.segmentsOptions).yields(null, { statusCode: '200' }, testVars.segmentsOutput);
-    stub.withArgs(testVars.countOptions).yields(null, { statusCode: '200' }, testVars.countsOutput);
-    done();
-  });
+describe('intercom api wrapper', function() {
+  describe('unit testing', function() {
+    before(function(done) {
+      var stub = sinon.stub(request, 'get');
+      stub.withArgs(testVars.usersOptions).yields(null, { statusCode: '200' }, testVars.usersOutput);
+      stub.withArgs(testVars.usersCreatedSinceOptions).yields(null, { statusCode: '200' }, testVars.usersOutput);
+      stub.withArgs(testVars.usersBySegmentOptions).yields(null, { statusCode: '200' }, testVars.usersOutput);
+      stub.withArgs(testVars.segmentsOptions).yields(null, { statusCode: '200' }, testVars.segmentsOutput);
+      stub.withArgs(testVars.countOptions).yields(null, { statusCode: '200' }, testVars.countsOutput);
+      done();
+    });
 
-  after(function(done) {
-    request.get.restore();
-    done();
-  });
+    after(function(done) {
+      request.get.restore();
+      done();
+    });
 
-  it('api should exist', function() {
-    expect(intercomApiWrapper).to.exist;
-  });
+    it('api should exist', function() {
+      expect(intercomApiWrapper).to.exist;
+    });
 
-  it('getUsers with no arguments should return a stream', function(done) {
-    intercomApiWrapper.getUsers(null, function(err, result) {
-      if (err) return done(err);
-      expect(result).to.be.an('object');
-      expect(result.__HighlandStream__).to.be.true;
-      var dataCounter = 0;
-      result.on('data', function(data) {
-        expect(data).to.equal(testVars.usersOutput[dataCounter]);
-        dataCounter++;
+    it('getUsers with no arguments should return a stream', function(done) {
+      intercomApiWrapper.getUsers(null, function(err, result) {
+        if (err) return done(err);
+        expect(result).to.be.an('object');
+        expect(result.__HighlandStream__).to.be.true;
+        var dataCounter = 0;
+        result.on('data', function(data) {
+          expect(data).to.equal(testVars.usersOutput.users[dataCounter]);
+          dataCounter++;
+        });
+        result.on('end', function() {
+          done();
+        });
       });
-      done();
     });
-  });
 
-  it('getUsers with lastNumDays should return a stream', function(done) {
-    intercomApiWrapper.getUsers(1, function(err, result) {
-      if (err) return done(err);
-      expect(result).to.be.an('object');
-      expect(result.__HighlandStream__).to.be.true;
-      result.on('data', function(data) {
-        data.to.equal(testVars.usersOutput);
+    it('getUsers with lastNumDays should return a stream', function(done) {
+      intercomApiWrapper.getUsers(1, function(err, result) {
+        if (err) return done(err);
+        expect(result).to.be.an('object');
+        expect(result.__HighlandStream__).to.be.true;
+        var dataCounter = 0;
+        result.on('data', function(data) {
+          expect(data).to.equal(testVars.usersOutput.users[dataCounter]);
+          dataCounter++;
+        });
+        result.on('end', function() {
+          done();
+        });
       });
-      done();
     });
-  });
 
-  it('getUsersBySegment with no segment should return no segment provided error', function(done) {
-    intercomApiWrapper.getUsersBySegment(null, function(err, result) {
-      expect(err).to.equal('NO SEGMENT PROVIDED');
-      expect(result).to.be.undefined;
-      done();
-    });
-  });
-
-  it('getUsersBySegment with bad segment should return segment does not exist error', function(done) {
-    intercomApiWrapper.getUsersBySegment('bad segment', function(err, result) {
-      expect(err).to.equal('SEGMENT PROVIDED DOES NOT EXIST');
-      expect(result).to.be.undefined;
-      done();
-    });
-  });
-
-  it('getUsersBySegment with correct segment should return a stream', function(done) {
-    intercomApiWrapper.getUsersBySegment('test', function(err, result) {
-      if (err) return done(err);
-      expect(result).to.be.an('object');
-      expect(result.__HighlandStream__).to.be.true;
-      var dataCounter = 0;
-      result.on('data', function(data) {
-        expect(data).to.equal(testVars.usersOutput[dataCounter]);
-        dataCounter++;
+    it('getUsersBySegment with no segment should return no segment provided error', function(done) {
+      intercomApiWrapper.getUsersBySegment(null, function(err, result) {
+        expect(err).to.equal('NO SEGMENT PROVIDED');
+        expect(result).to.be.undefined;
+        done();
       });
-      done();
     });
-  });
 
-  it('getCount with no type should return no type provided error', function(done) {
-    intercomApiWrapper.getCount(null, 'test', function(err, result) {
-      expect(err).to.equal('NO TYPE OR COUNT PROVIDED');
-      expect(result).to.be.undefined;
-      done();
-    });
-  });
-
-  it('getCount with no count should return no count provided error', function(done) {
-    intercomApiWrapper.getCount('test', null, function(err, result) {
-      expect(err).to.equal('NO TYPE OR COUNT PROVIDED');
-      expect(result).to.be.undefined;
-      done();
-    });
-  });
-
-  it('getCount with correct type and count should return a stream', function(done) {
-    intercomApiWrapper.getCount('user', 'segment', function(err, result) {
-      if (err) return done(err);
-      expect(result).to.be.an('object');
-      expect(result.__HighlandStream__).to.be.true;
-      var dataCounter = 0;
-      result.on('data', function(data) {
-        expect(data).to.equal(testVars.countsOutput[dataCounter]);
-        dataCounter++;
+    it('getUsersBySegment with bad segment should return segment does not exist error', function(done) {
+      intercomApiWrapper.getUsersBySegment('bad segment', function(err, result) {
+        expect(err).to.equal('SEGMENT PROVIDED DOES NOT EXIST');
+        expect(result).to.be.undefined;
+        done();
       });
-      done();
+    });
+
+    it('getUsersBySegment with correct segment should return a stream', function(done) {
+      intercomApiWrapper.getUsersBySegment('test', function(err, result) {
+        if (err) return done(err);
+        expect(result).to.be.an('object');
+        expect(result.__HighlandStream__).to.be.true;
+        var dataCounter = 0;
+        result.on('data', function(data) {
+          expect(data).to.equal(testVars.usersOutput.users[dataCounter]);
+          dataCounter++;
+        });
+        result.on('end', function() {
+          done();
+        });
+      });
+    });
+
+    it('getCount with no type should return no type provided error', function(done) {
+      intercomApiWrapper.getCount(null, 'test', function(err, result) {
+        expect(err).to.equal('NO TYPE OR COUNT PROVIDED');
+        expect(result).to.be.undefined;
+        done();
+      });
+    });
+
+    it('getCount with no count should return no count provided error', function(done) {
+      intercomApiWrapper.getCount('test', null, function(err, result) {
+        expect(err).to.equal('NO TYPE OR COUNT PROVIDED');
+        expect(result).to.be.undefined;
+        done();
+      });
+    });
+
+    it('getCount with correct type and count should return a stream', function(done) {
+      intercomApiWrapper.getCount('user', 'segment', function(err, result) {
+        if (err) return done(err);
+        expect(result).to.be.an('object');
+        expect(result.__HighlandStream__).to.be.true;
+        var dataCounter = 0;
+        result.on('data', function(data) {
+          expect(data).to.equal(testVars.countsOutput.user.segment[dataCounter]);
+          dataCounter++;
+        });
+        result.on('end', function() {
+          done();
+        });
+      });
+    });
+  });
+  describe('functional testing', function() {
+    it('should connect to server and return a stream', function(done) {
+      intercomApiWrapper.getUsers(1, function(err, result) {
+        if (err) return done(err);
+        expect(result).to.be.an('object');
+        expect(result.__HighlandStream__).to.be.true;
+
+        result.on('data', function(data) {
+          expect(data).to.have.property('type', 'user');
+          expect(data.id).to.exist;
+          expect(data.user_id).to.exist;
+        });
+        result.on('end', function() {
+          done();
+        });
+      });
+    });
+
+    it('should connect to server and return a stream', function(done) {
+      intercomApiWrapper.getCount('user', 'segment', function(err, result) {
+        if (err) return done(err);
+        expect(result).to.be.an('object');
+        expect(result.__HighlandStream__).to.be.true;
+        result.on('data', function(data) {
+          expect(data).to.be.an('object');
+        });
+        result.on('end', function() {
+          done();
+        });
+      });
     });
   });
 });
